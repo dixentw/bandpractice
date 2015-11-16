a66fb20f703583ae292a4fd99f99ca9b8c8e06f5
Dixentw <dixentw@gmail.com>
2015-11-16 09:55:08 +0800

    Initial Commit

---
+++ BandPractice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BandPractice
+//
+//  Created by Dixen Cheng on 11/16/15.
+//  Copyright © 2015 Dixen Cheng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
